08dd4f0850f09e533ecdd8edc477de60b8a36e9c
Peter Vereshagin <peter@vereshagin.org>
2012-10-04 14:58:06 +0400

    initial commit

---
+++ lib/Spunge/DB.pm
@@ -0,0 +1,41 @@
+# ABSTRACT: (Description goes here)
+package Spunge::DB;
+
+# VERSION
+
+# Helps you to behave
+use strict;
+use warnings;
+
+### MODULES ###
+#
+
+### CONSTANTS ###
+#
+
+### ATTRIBUTES ###
+#
+
+### SUBS ###
+#
+
+# Returns true to require()
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SUBROUTINES/METHODS
+
+=head1 DIAGNOSTICS
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=cut

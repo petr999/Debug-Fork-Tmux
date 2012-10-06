63326433aed33c65cfe6f648fc55ba00b82f817f
Peter Vereshagin <peter@vereshagin.org>
2012-10-07 01:44:03 +0400

    WORKSFORME
    Working code is created:
    
      - the module Spunge::DB with subs, docs, etc.
      - tests include:
    
        - 10-use.t tries to use the module. No bad if it only require()s the
          module instead of use() as module does not @EXPORT anything;
        - 20-cmd.t tries the basic commands and the module's built-in exception
          catching;
        - 30-tmux.t tries to call functions executing the Tmux.

--- lib/Spunge/DB.pm
+++ lib/Spunge/DB.pm
@@ -9,15 +9,172 @@ use warnings;
 
 ### MODULES ###
 #
+# Glues up path components
+use File::Spec;
+
+# Resolves up symlinks
+use Cwd;
+
+# Dioes in a nicer way
+use Carp;
+
+# Makes constants possible
+use Const::Fast;
 
 ### CONSTANTS ###
 #
+# Path to the 'tmux' binary
+# Requires  :   Cwd
+const my $TMUX_PATH => Cwd::realpath('/usr/local/bin');
 
-### ATTRIBUTES ###
-#
+# 'tmux' binary itself
+# Depends   :   On $TMUX_PATH
+# Requires  :   File::Spec
+const my $TMUX_BIN => File::Spec->catfile( $TMUX_PATH => 'tmux' );
+
+# 'tmux' command for a new window returning the 'tmux id' for the window
+const my @TMUX_CMD_NEWW =>
+    ( $TMUX_BIN => ( 'new-window' => '-P', 'sleep 1000000' ), );
+
+# 'tmux' command for output of a tty name of the window supplied.
+const my @TMUX_CMD_TTY =>
+    ( $TMUX_BIN => ( qw/lsp -F/, '#{pane_tty}', '-t' ) );
 
 ### SUBS ###
 #
+# Function
+# Gets the tty name, sets the $DB::fork_TTY to it and returns it.
+# Takes     :   n/a
+# Requires  :   DB, Spunge::DB
+# Overrides :   DB::get_fork_TTY()
+# Changes   :   $DB::fork_TTY
+# Returns   :   Str tty name $DB::fork_TTY
+sub DB::get_fork_TTY {
+
+    # Create a TTY
+    my $tty_name = Spunge::DB::spawn_tty();
+
+    # Output the name both to a variable and to the caller
+    no warnings qw/once/;
+    $DB::fork_TTY = $tty_name;
+    return $tty_name;
+}
+
+# Function
+# Spawns a TTY and returns its name
+# Takes     :   n/a
+# Returns   :   Str tty name
+sub spawn_tty {
+
+    # Create window and get its tty name
+    my $window_id = tmux_new_window();
+    my $tty_name  = tmux_window_tty($window_id);
+
+    return $tty_name;
+}
+
+# Function
+# Creates new 'tmux' window  and returns its id/number
+# Takes     :   n/a
+# Depends   :   On @TMUX_CMD_NEWW package lexical
+# Returns   :   Str id/number of the created 'tmux' window
+sub tmux_new_window {
+    my $window_id = read_from_cmd(@TMUX_CMD_NEWW);
+
+    return $window_id;
+}
+
+# Function
+# Gets a 'tty' name from 'tmux's window id/number
+# Takes     :   Str 'tmux' window id/number
+# Depends   :   On @TMUX_CMD_TTY package lexical
+# Returns   :   Str 'tty' device name of the 'tmux' window
+sub tmux_window_tty {
+    my $window_id = shift;
+
+    # Concatenate the 'tmux' command and read its output
+    my @tmux_cmd = ( @TMUX_CMD_TTY, $window_id );
+    my $tty_name = read_from_cmd(@tmux_cmd);
+
+    return $tty_name;
+}
+
+# Function
+# Reads the output of a command supplied with parameters as the argument(s)
+# and returns its output.
+# Takes     :   Array[Str] command and its parameters
+# Throws    :   If command failed or the output is not the non-empty Str
+#               single line
+# Returns   :   Output of the command supplied with parameters as arguments
+sub read_from_cmd {
+    my @cmd_and_args = @_;
+
+    # Open the pipe to read
+    croak_on_cmd( @cmd_and_args, "failed opening command: $!" )
+        unless open my $cmd_output_fh => '-|',
+        @cmd_and_args;
+
+    # Read a line from the command
+    croak_on_cmd( @cmd_and_args, "didn't write a line" )
+        unless defined($cmd_output_fh)
+        and ( 0 != $cmd_output_fh )
+        and my $cmd_out = <$cmd_output_fh>;
+
+    # If still a byte is readable then die as the file handle should be
+    # closed already
+    my $read_rv = read $cmd_output_fh => my $buf, 1;
+    croak_on_cmd( @cmd_and_args, "failed reading command: $!" )
+        unless defined $read_rv;
+    croak_on_cmd( @cmd_and_args, "did not finish" ) unless 0 == $read_rv;
+
+    # Die on empty output
+    chomp $cmd_out;
+    croak_on_cmd( @cmd_and_args, "provided empty string" )
+        unless length $cmd_out;
+
+    return $cmd_out;
+}
+
+# Function
+# Croaks nicely on the command with an explanation based on arguments and $?
+# Takes     :   Array[Str] system command, its arguments, and an explanation
+#               on the situation when the command failed
+# Requires  :   Carp
+# Depends   :   On $? global variable set by system command failure
+# Throws    :   Always
+# Returns   :   n/a
+sub croak_on_cmd {
+    my @cmd_args_msg = @_;
+
+    if ( defined $? ) {
+        my $msg = '';
+
+        # Depending on $?, add it to the death note
+        # Command may be a not-executable
+        if ( $? == -1 ) {
+            $msg = "failed to execute: $!";
+        }
+
+        # Command can be killed
+        elsif ( $? & 127 ) {
+            $msg = sprintf "child died with signal %d, %s coredump",
+                ( $? & 127 ), ( $? & 128 ) ? 'with' : 'without';
+        }
+
+        # Command may return the exit code for clearance
+        else {
+            $msg = sprintf "child exited with value %d", $? >> 8;
+        }
+
+        # And the message can be returned as an appendix to the original
+        # arguments
+        push @cmd_args_msg, $msg;
+    }
+
+    # Report the datails via the Carp
+    my $croak_msg = "The command " . join ' ' => @cmd_args_msg;
+    croak($croak_msg);
+}
 
 # Returns true to require()
 1;
@@ -38,10 +195,103 @@ You should run it from inside the C<tmux> window manager.
 
 =head1 DESCRIPTION
 
+The Perl's standard debugger requires additional stuff when the debugged
+Perl program use the L<fork()> built-in.
+
+This module is about to solve the trouble which visible like this:
+
+  ######### Forked, but do not know how to create a new TTY. #########
+  Since two debuggers fight for the same TTY, input is severely entangled.
+
+  I know how to switch the output to a different window in xterms, OS/2
+  consoles, and Mac OS X Terminal.app only.  For a manual switch, put the
+  name of the created TTY in $DB::fork_TTY, or define a function
+  DB::get_fork_TTY() returning this.
+
+  On UNIX-like systems one can get the name of a TTY for the given window
+  by typing tty, and disconnect the shell from TTY by sleep 1000000.
+
+C<OS/2> is a fun for me but in the past. C<Mac OS X> is a more chance but
+still isn't real. And for C<xterm> ... who wants to keep it on your server?
+Gimme some stones to throw on that one.
+
+But the pseudo-terminal device isn't much about C<GUI>s by its nature so the
+problem behind the bars of the L<perl5db.pl> report ( see more detailed
+problem description at the L<PerlMonks
+thread|http://perlmonks.org/?node_id=128283> ) is the consoles management.
+It's a kind of a tricky, for example, to start the next C<ssh> session
+initiated from the machine serving as an C<sshd> server for the existing
+session.
+
+Thus we kind of have to give a chance to the consoles management with
+a software capable to run on a server machine without as much dependencies
+as an C<xterm>. This module is a try to pick the L<Tmux|http://tmux.sf.net>
+windows manager for such a task.
+
+=head1 CONSTANTS
+
+=const C<$TMUX_PATH>
+
+Path to the C<tmux> binary.
+
+=const C<$TMUX_BIN>
+
+The C<tmux> binary fully qualified file name.
+
+=const C<@TMUX_CMD_NEWW>
+
+The L<system> arguments for a C<tmux> command for opening a new window and
+with output of a window address in C<tmux>.
+
+=const C<@TMUX_CMD_TTY>
+
+The L<system> arguments for a  C<tmux> command for finding a C<tty> name in
+the output.  Expects C<tmux>'s window address as the very last argument.
+
 =head1 SUBROUTINES/METHODS
+
+All of the following are functions:
+
+=pubsub C<DB::get_fork_TTY()>
+
+Finds new C<TTY> for the C<fork()>ed process.
+
+Takes no arguments. Returns C<Str> name of the C<tty> device of the <tmux>'s
+new window created for the debugger's new process.
+
+Sets the $DB::fork_TTY to the same <Str> value.
+
+=sub C<spawn_tty>
+
+Creates a C<TTY> device and returns Str its name.
+
+=sub C<tmux_new_window()>
+
+Creates a C<tmux> window and returns Str its id/number.
+
+=sub C<tmux_window_tty()>
+
+Checks for a C<tmux> window's tty name and returns its Str name.
+
+=sub C<read_from_cmd>
+
+Takes the list containing the L<system> command and its arguments and
+executes it. Reads Str the output and returns it. Throws if no output or if
+the command failed.
+
+=sub C<croak_on_cmd>
+
+Takes the command and the reason of its failure, examines the $? and dies
+with explanation on the L<system> command failure.
 
 =head1 DIAGNOSTICS
 
 =head1 CONFIGURATION AND ENVIRONMENT
+
+The module requires the L<Tmux|http://tmux.sf.net> window manager for the
+console to be present in the system.
+
+For some while, the configuration is made via the package lexical
+constants.
 
 =cut
---
+++ t/30-tmux.t
@@ -0,0 +1,141 @@
+#!/usr/bin/env perl
+# Tests tmux-related stuff for Spunge::DB.
+#
+# Copyright (C) 2012 Peter Vereshagin <peter@vereshagin.org>
+# All rights reserved.
+#
+
+# Helps you to behave
+use strict;
+use warnings;
+
+# Throws exceptions on i/o
+use autodie;
+
+### MODULES ###
+#
+# Makes this test a test
+use Test::Most qw/bail/;    # BAIL_OUT() on any failure
+
+# Loads main app module
+use Spunge::DB;
+
+# Catches exceptions
+use Test::Exception;
+
+# Can compare version numbers
+use Sort::Versions;
+
+### CONSTANTS ###
+#
+# Makes constants possible
+use Const::Fast;
+
+# Match 'tmux -V' to get a version number
+const my $TMUX_VERSION_RGX => qr/^(.*[-\s])?((\d+\.\d+)(\.\d+)*)$/;
+
+# Minimum tmux version for Spunge::DB to work with
+const my $TMUX_MIN_VERSION => 1.6;
+
+# The what the device name returned from Tmuxs hould look like
+const my $TMUX_TTY_RGX =>
+    qr/^(([\w\d\/]*[\w\d]\/)?\w+ty[\w\d]+)|((\/dev\/pts\/)?\d+)$/;
+
+### MAIN ###
+# Require   :   Test::Most, Test::Exception
+#
+# Determine if it's not under tmux
+# Depends   :   On %ENV global
+is( $ENV{'TERM'} => 'screen', 'Appropriate environment for tmux' );
+lives_ok { system "tmux -V 2>&1 > /dev/null" } 'tmux is found in the system';
+
+# Reads output of 'tmux' for various situations
+lives_and {
+
+    my ( $buf_str => @buf_strings, $fh );
+
+    # Read version number
+    open $fh => '-|', 'tmux' => '-V';    # autodie
+    $buf_str = do { local $/ = undef; <$fh>; };    # autodie
+    close $fh;
+
+    # Read the version
+    # Depends   :   On Test::Most->import qw/bail/ );
+    @buf_strings = split /\r*\n\r*/, $buf_str;
+    is( @buf_strings => 1,
+        "The command 'tmux -V' provides a single line of the text"
+    );
+    $buf_str = shift @buf_strings;
+    ok( defined($buf_str) =>
+            "The command 'tmux -V' provides a defined value" );
+    chomp $buf_str;
+    isnt(
+        length($buf_str) => 0,
+        "The command 'tmux -V' outputs a non-empty string"
+    );
+    like(
+        $buf_str => $TMUX_VERSION_RGX,
+        "The command 'tmux -V' matches a version regex"
+    );
+
+    # Ensure the minimum Tmux version requirement
+    # $2 is defined according to regex
+    # Requires  :   Sort::Versions
+    $buf_str =~ $TMUX_VERSION_RGX;    # tested with like() already
+    my $version = $2;
+    isnt(
+        Sort::Versions::versioncmp( $version => $TMUX_MIN_VERSION ) => -1,
+        "Tmux version '$version' is not less than the minimum"
+            . " '$TMUX_MIN_VERSION'"
+    );
+
+    # Read session info
+    open $fh => '-|', 'tmux' => 'info';    # autodie
+    $buf_str = do { local $/ = undef; <$fh>; };    # autodie
+    close $fh;
+
+    @buf_strings = split /\r*\n\r*/, $buf_str;
+    cmp_ok( @buf_strings, '>', 1,
+        "The command 'tmux info' provides several lines of the text" );
+}
+'Tmux commands execution';
+
+lives_and {
+    my ( $window_id => $window_tty );
+
+    # Create window and kill it
+    ok( $window_id
+            = Spunge::DB::read_from_cmd( qw/tmux neww -P/,
+            'sleep 1000000' ) => "Created window: $window_id" );
+    ok( length($window_id) => 'window id is not empty' );
+    ok( $window_tty = Spunge::DB::read_from_cmd(
+            qw/tmux lsp -F/ => '#{pane_tty}',
+            '-t'            => $window_id,
+        ) => "Found a tty $window_tty for a Tmux window: $window_id"
+    );
+
+    system( qw/tmux killw -t/, $window_id );
+    is( ${^CHILD_ERROR_NATIVE} => 0, "killed window: $window_id" );
+
+    # Create window with Spounge::DB and kill it
+    ok( $window_id = Spunge::DB::tmux_new_window(),
+        "Spunge::DB created a Tmux window: $window_id",
+    );
+    ok( $window_tty = Spunge::DB::tmux_window_tty($window_id),
+        "Spunge::DB found a tty $window_tty for a Tmux window: $window_id",
+    );
+    ok( length($window_id)  => 'window id is not empty' );
+    ok( length($window_tty) => 'window tty is not empty' );
+    like(
+        $window_tty => $TMUX_TTY_RGX,
+        "window tty '$window_tty' looks like a pseudo-terminal device"
+    );
+
+    system( qw/tmux killw -t/, $window_id );
+    is( ${^CHILD_ERROR_NATIVE} => 0, "killed window: $window_id" );
+}
+'Tmux window manipulation';
+
+# Continues till this point
+# Requires  :   Test::Most
+done_testing();
